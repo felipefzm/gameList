d5dab2087f3cbfc228238b28562ee11e20d77979
Felipe Módolo <felipegbr14@gmail.com>
2025-01-24 02:29:27 -0300

    Criação do Projeto

---
+++ src/main/java/com/felipe/gamelist/GamelistApplication.java
@@ -0,0 +1,13 @@
+package com.felipe.gamelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamelistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamelistApplication.class, args);
+	}
+
+}
